743d2bebd9759c7ae88e708109d438eec57de9a7
Felipe Moreno <85708817+blacklottus-09@users.noreply.github.com>
2025-06-03 08:16:45 +0000

    the integration with the api was added

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,19 +1,101 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+const urlBase = 'https://playground.4geeks.com/todo';
+
+const initialStateTask = {
+  label: "",
+  is_done: false
+};
+const initialError = {
+  message: "",
+  type: ""
+}
 
 const Home = () => {
-  const [task, setTask] = useState("");
+  const [task, setTask] = useState(initialStateTask);
   const [taskList, setTaskList] = useState([]);
+  const [error, setError] = useState(initialError)
 
-  const addTask = (event) => {
-    if (event.key === "Enter" && task.trim() !== "") {
-      setTaskList([...taskList, task]); 
-      setTask("");
+  const createUser = async () => {
+    try {
+      const response = await fetch(`${urlBase}/users/test123`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      if (response.ok) {
+        getAllTask()
+      }
+    } catch {
+      console.log(error)
+    }
+  }
+
+  const getAllTask = async () => {
+    try {
+      const response = await fetch(`${urlBase}/users/test123`);
+      const data = await response.json();
+      if (response.status == 404) {
+        createUser()
+      }
+      else if (response.ok) {
+        setTaskList(data.todos);
+      } else {
+        console.log('ah ah ah no dijiste la palabra mágica');
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
-  const deleteTask = (indexToDelete) => {
-    setTaskList(taskList.filter((_, index) => index !== indexToDelete));
+  const addTask = async (event) => {
+    if (event.key === "Enter" && task.label.trim() !== "") {
+      try {
+        const response = await fetch(`${urlBase}/todos/test123`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(task)
+        });
+        if (response.ok) {
+          getAllTask();
+          setTask(initialStateTask);
+        }
+      } catch (error) {
+        console.log('Error al agregar tarea:', error);
+      }
+    }
   };
+
+  const deleteTask = async (id) => {
+    try {
+      const response = await fetch(`${urlBase}/todos/${id}`, { method: "DELETE" })
+      if (response.ok) {
+        getAllTask()
+      }
+    } catch {
+      console.log(error)
+    }
+  };
+  const deleteUser = async () => {
+    try {
+      const response = await fetch(`${urlBase}/users/test123`, {
+        method: "DELETE"
+      })
+      if (response.ok) {
+        getAllTask()
+      }
+    } catch {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    createUser()
+    getAllTask();
+  }, []);
 
   return (
     <div className="container py-5">
@@ -25,23 +107,23 @@ const Home = () => {
             type="text"
             className="form-control border-0 fs-4 py-3"
             placeholder="What needs to be done?"
-            value={task}
-            onChange={(e) => setTask(e.target.value)}
+            value={task.label}
+            onChange={(e) => setTask({ ...task, label: e.target.value })}
             onKeyDown={addTask}
           />
           <ul className="list-group list-group-flush">
             {taskList.length === 0 ? (
               <li className="list-group-item text-center text-muted">No tasks, add a task</li>
             ) : (
-              taskList.map((taskToDo, index) => (
+              taskList.map((item) => (
                 <li
-                  key={index}
+                  key={item.id}
                   className="list-group-item d-flex justify-content-between align-items-center task-item fs-4"
                 >
-                  <span className="flex-grow-1">{taskToDo}</span>
+                  <span className="flex-grow-1">{item.label}</span>
                   <button
                     className="btn btn-sm btn-outline-danger delete-btn ms-3"
-                    onClick={() => deleteTask(index)}
+                    onClick={() => deleteTask(item.id)}
                   >
                     ✖
                   </button>
@@ -51,6 +133,11 @@ const Home = () => {
           </ul>
           <div className="text-muted px-3 py-2 small">
             {taskList.length} item{taskList.length !== 1 ? "s" : ""} left
+          </div>
+          <div className="d-flex justify-content-center">
+            <button
+              className="btn btn-danger"
+              onClick={deleteUser}>Delete all task</button>
           </div>
         </div>
       </div>
